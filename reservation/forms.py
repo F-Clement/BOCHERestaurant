7f91b3c92dab9963e4a72fad6674fad72a88ad9e
F-Clement <>
2023-11-23 22:28:40 +0000

    Adjust css on several pages for consistency

--- reservation/forms.py
+++ reservation/forms.py
@@ -11,4 +11,16 @@ class ReservationForm(forms.ModelForm):
                   'number_people', 'reservation_date', 'reservation_time']
 
         widgets = {'reservation_date': forms.DateInput(
-            attrs={'type': 'date'})}
+            attrs={'type': 'date', 'placeholder': 'reservation date'}),
+            'reservation_name': forms.TextInput(attrs={
+                'placeholder': 'Reservation name'
+            }),
+            'reservation_email': forms.TextInput(attrs={
+                'placeholder': 'Reservation email'
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'placeholder': 'Phone number'
+            }),
+            'number_people': forms.TextInput(attrs={
+                'placeholder': 'Number of people'
+            })}
