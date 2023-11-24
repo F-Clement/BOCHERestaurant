74e4b7145611c66fbf7b90eb5e0ef2a2fbb9d5f5
F-Clement <>
2023-11-24 15:47:23 +0000

    Remove empty line spaces from code and fix nav urls

--- reservation/urls.py
+++ reservation/urls.py
@@ -5,14 +5,14 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePage.as_view(),
          name='homepage'),
-    path('menu/', views.Menu.as_view(), name='Menu'),
+    path('menu/', views.Menu.as_view(), name='menu'),
     path('add_reservation', views.AddReservation.as_view(
-        success_url="/reservations"), name='new reservation'),
+        success_url="/reservations"), name='new_reservation'),
     path('reservations/', views.ReservationPage.as_view(),
-         name='list reservation'),
+         name='list_reservation'),
     path('edit_reservation/<int:pk>', views.EditReservation.as_view(
-        success_url="/reservations")),
-    path('delete_reservation/<int:pk>',
-         views.DeleteReservation.as_view(success_url="/reservations"))
+        success_url="/reservations"), name='edit_reservation'),
+    path('delete_reservation/<int:pk>', views.DeleteReservation.as_view(
+        success_url="/reservations"), name='delete_reserv')
 
 ]
